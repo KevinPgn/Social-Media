562e22bc64896c9415c205f2f597109b07e02adf
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-01 16:41:37 +0200

    animation

--- src/components/headers/Headers.tsx
+++ src/components/headers/Headers.tsx
@@ -6,7 +6,8 @@ import { Profile } from "./Profile"
 export const Headers = () => {
   return <header className="w-full h-20 shadow-xl bg-[#141414] px-3">
     <nav className="max-w-[1400px] mx-auto h-full flex items-center justify-between">
-        <h2 className="text-xl uppercase text-pink-500 font-semibold">
+        <h2 className="text-xl uppercase text-pink-500 font-semibold
+         hover:animate-gradient duration-75">
             <Link href="/">KevinPgn</Link>
         </h2>
         <Links />
