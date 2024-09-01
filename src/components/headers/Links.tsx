f8ef7f22d3650b77141ef2cf96283d33c437d0b5
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-01 16:38:30 +0200

    headers

--- src/components/headers/Links.tsx
+++ src/components/headers/Links.tsx
@@ -1,17 +1,21 @@
+"use client"
 import Link from "next/link"
 import {Home, Users, Users2} from "lucide-react"
+import { usePathname } from "next/navigation"
+import { cn } from "@/lib/utils"
 
 export const Links = () => {
+  const pathname = usePathname()
   return <div className="flex items-center gap-7">
-    <Link href="/" className="flex hover:text-pink-500 duration-75 items-center gap-2 text-sm">
+    <Link href="/" className={cn("flex hover:text-pink-500 duration-75 items-center gap-2 text-sm", pathname === "/" && "text-pink-500")}>
       <Home size={18} />
       <span>Homepage</span>
     </Link>
-    <Link href="/friends" className="flex hover:text-pink-500 duration-75 items-center gap-2 text-sm">
+    <Link href="/friends" className={cn("flex hover:text-pink-500 duration-75 items-center gap-2 text-sm", pathname === "/friends" && "text-pink-500")}>
       <Users size={18} />
       <span>Friends</span>
     </Link>
-    <Link href="/community" className="flex hover:text-pink-500 duration-75 items-center gap-2 text-sm">
+    <Link href="/community" className={cn("flex hover:text-pink-500 duration-75 items-center gap-2 text-sm", pathname === "/community" && "text-pink-500")}>
       <Users2 size={18} />
       <span>Community</span>
     </Link>
