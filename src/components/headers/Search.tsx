636d5b76b3794b6276ee3b6705f1c6638b17ecd4
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-01 16:50:15 +0200

    headers

--- src/components/headers/Search.tsx
+++ src/components/headers/Search.tsx
@@ -3,8 +3,8 @@ import { Search as SearchIcon } from "lucide-react"
 import { Input } from "@/components/ui/input"
 
 export const Search = () => {
-  return <div className="hidden lg:flex bg-[#121212] w-[220px] items-center gap-2 px-3 rounded-sm">
-    <Input placeholder="Search" className="bg-transparent border-none" />
+  return <div className="hidden lg:flex bg-[#121212] w-[220px] max-md:w-full items-center gap-2 px-3 rounded-sm">
+    <Input placeholder="Search..." className="bg-transparent text-sm border-none" />
     <SearchIcon size={18} className="text-white/50 cursor-pointer"/>
   </div>
 }
